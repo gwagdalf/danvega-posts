3f63d45988d40b5cda345f6aea8fca900ff81c05
곽형섭 <hgwag@gmarket.com>
2023-12-23 21:44:37 +0900

    lombok 추가, 구분위해 testContainer 는 16.1 로 버전 변경

--- src/test/java/dev/danvega/danson/post/PostControllerIntTest.java
+++ src/test/java/dev/danvega/danson/post/PostControllerIntTest.java
@@ -1,5 +1,6 @@
 package dev.danvega.danson.post;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,87 +19,115 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Slf4j
 @Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Transactional
 class PostControllerIntTest {
 
-    @Container
-    @ServiceConnection
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16.0");
+  @Container
+  @ServiceConnection
+  static PostgreSQLContainer<?> postgres =
+      new PostgreSQLContainer<>("postgres:16.1");
 
-    @Autowired
-    TestRestTemplate restTemplate;
 
-    @Test
-    void connectionEstablished() {
-        assertThat(postgres.isCreated()).isTrue();
-        assertThat(postgres.isRunning()).isTrue();
-    }
 
-    @Test
-    void shouldFindAllPosts() {
-        Post[] posts = restTemplate.getForObject("/api/posts", Post[].class);
-        assertThat(posts.length).isGreaterThan(100);
-    }
-    
-    @Test
-    void shouldFindPostWhenValidPostID() {
-        ResponseEntity<Post> response = restTemplate.exchange("/api/posts/1", HttpMethod.GET, null, Post.class);
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getBody()).isNotNull();
-    }
+  @Autowired
+  TestRestTemplate restTemplate;
 
-    @Test
-    void shouldThrowNotFoundWhenInvalidPostID() {
-        ResponseEntity<Post> response = restTemplate.exchange("/api/posts/999", HttpMethod.GET, null, Post.class);
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
-    }
+  @Test
+  void connectionEstablished() {
+    assertThat(postgres.isCreated()).isTrue();
+    assertThat(postgres.isRunning()).isTrue();
 
-    @Test
-    @Rollback
-    void shouldCreateNewPostWhenPostIsValid() {
-        Post post = new Post(101,1,"101 Title","101 Body",null);
+    log.info(STR."### isRunning: \{postgres.isRunning()}");
+    log.info(STR."### getMappedPort: \{postgres.getMappedPort(5432)}");
+    log.info(STR."### getLivenessCheckPorts : \{postgres.getLivenessCheckPortNumbers()}");
+    log.info(STR."### getExposedPorts: \{postgres.getExposedPorts()}");
 
-        ResponseEntity<Post> response = restTemplate.exchange("/api/posts", HttpMethod.POST, new HttpEntity<Post>(post), Post.class);
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-        assertThat(response.getBody()).isNotNull();
-        assertThat(Objects.requireNonNull(response.getBody()).id()).isEqualTo(101);
-        assertThat(response.getBody().userId()).isEqualTo(1);
-        assertThat(response.getBody().title()).isEqualTo("101 Title");
-        assertThat(response.getBody().body()).isEqualTo("101 Body");
-    }
+    log.info(STR."### getUsername : \{postgres.getUsername()}");
+    log.info(STR."### getPassword  : \{postgres.getPassword()}");
+    log.info(STR."### getDatabaseName  : \{postgres.getDatabaseName()}");
+    log.info(STR."### getDriverClassName : \{postgres.getDriverClassName()}");
 
-    @Test
-    void shouldNotCreateNewPostWhenValidationFails() {
-        Post post = new Post(101,1,"","",null);
-        ResponseEntity<Post> response = restTemplate.exchange("/api/posts", HttpMethod.POST, new HttpEntity<Post>(post), Post.class);
+    log.info(STR."### getJdbcUrl  : \{postgres.getJdbcUrl()}");
+    log.info(STR."### getTestQueryString : \{postgres.getTestQueryString()}");
 
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
-    }
+    log.info(STR."### getHost: \{postgres.getHost()}");
+    log.info(STR."### getContainerId: \{postgres.getContainerId()}");
+    log.info(STR."### getContainerInfo: \{postgres.getContainerInfo()}");
 
-    @Test
-    @Rollback
-    void shouldUpdatePostWhenPostIsValid() {
-        ResponseEntity<Post> response = restTemplate.exchange("/api/posts/99", HttpMethod.GET, null, Post.class);
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    String containerLogs = postgres.getLogs();
+    log.info(STR."### getLogs: \{containerLogs}");
+    log.info("### log ends");
 
-        Post existing = response.getBody();
-        assertThat(existing).isNotNull();
-        Post updated = new Post(existing.id(),existing.userId(),"NEW POST TITLE #1", "NEW POST BODY #1",existing.version());
+    assertTrue(containerLogs.contains("PostgreSQL init process complete"));
+  }
 
-        assertThat(updated.id()).isEqualTo(99);
-        assertThat(updated.userId()).isEqualTo(10);
-        assertThat(updated.title()).isEqualTo("NEW POST TITLE #1");
-        assertThat(updated.body()).isEqualTo("NEW POST BODY #1");
-    }
+  @Test
+  void shouldFindAllPosts() {
+    Post[] posts = restTemplate.getForObject("/api/posts", Post[].class);
+    assertThat(posts.length).isGreaterThan(100);
+  }
 
-    @Test
-    @Rollback
-    void shouldDeleteWithValidID() {
-        ResponseEntity<Void> response = restTemplate.exchange("/api/posts/88", HttpMethod.DELETE, null, Void.class);
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
-    }
+  @Test
+  void shouldFindPostWhenValidPostID() {
+    ResponseEntity<Post> response = restTemplate.exchange("/api/posts/1", HttpMethod.GET, null, Post.class);
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    assertThat(response.getBody()).isNotNull();
+  }
+
+  @Test
+  void shouldThrowNotFoundWhenInvalidPostID() {
+    ResponseEntity<Post> response = restTemplate.exchange("/api/posts/999", HttpMethod.GET, null, Post.class);
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+  }
+
+  @Test
+  @Rollback
+  void shouldCreateNewPostWhenPostIsValid() {
+    Post post = new Post(101, 1, "101 Title", "101 Body", null);
+
+    ResponseEntity<Post> response = restTemplate.exchange("/api/posts", HttpMethod.POST, new HttpEntity<Post>(post), Post.class);
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+    assertThat(response.getBody()).isNotNull();
+    assertThat(Objects.requireNonNull(response.getBody()).id()).isEqualTo(101);
+    assertThat(response.getBody().userId()).isEqualTo(1);
+    assertThat(response.getBody().title()).isEqualTo("101 Title");
+    assertThat(response.getBody().body()).isEqualTo("101 Body");
+  }
+
+  @Test
+  void shouldNotCreateNewPostWhenValidationFails() {
+    Post post = new Post(101, 1, "", "", null);
+    ResponseEntity<Post> response = restTemplate.exchange("/api/posts", HttpMethod.POST, new HttpEntity<Post>(post), Post.class);
+
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+  }
+
+  @Test
+  @Rollback
+  void shouldUpdatePostWhenPostIsValid() {
+    ResponseEntity<Post> response = restTemplate.exchange("/api/posts/99", HttpMethod.GET, null, Post.class);
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+    Post existing = response.getBody();
+    assertThat(existing).isNotNull();
+    Post updated = new Post(existing.id(), existing.userId(), "NEW POST TITLE #1", "NEW POST BODY #1", existing.version());
+
+    assertThat(updated.id()).isEqualTo(99);
+    assertThat(updated.userId()).isEqualTo(10);
+    assertThat(updated.title()).isEqualTo("NEW POST TITLE #1");
+    assertThat(updated.body()).isEqualTo("NEW POST BODY #1");
+  }
+
+  @Test
+  @Rollback
+  void shouldDeleteWithValidID() {
+    ResponseEntity<Void> response = restTemplate.exchange("/api/posts/88", HttpMethod.DELETE, null, Void.class);
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+  }
 
 }
